f51c3c91b96de21f22178f3ee690e8cf1cf66a88
cjponter <33551436+cjponter@users.noreply.github.com>
2018-02-24 15:19:49 +0000

    Add files via upload

--- DB Setup.sql
+++ DB Setup.sql
@@ -3,22 +3,25 @@ CREATE DATABASE IF NONE EXISTS ItemStorage;
 USE ItemStorage;
 
 CREATE TABLE IF NONE EXISTS [User] (
-    UserID int,
-    UserName varchar(20)
-    )
+    UserID int NOT NULL AUTO_INCREMENT,
+    UserName varchar(20),
+    PRIMARY KEY (UserID)
+);
 
 CREATE TABLE IF NONE EXISTS Product (
-    ProdID int,
+    ProdID int NOT NULL AUTO_INCREMENT,
     ProdNAME varchar(30),
     ProdType varchar(20),
-    ProdDescription varchar(100)
-)
+    ProdDescription varchar(100),
+    PRIMARY KEY (ProdID)
+);
 
 CREATE TABLE IF NONE EXISTS Cupboard (
-    CupID int,
+    CupID int NOT NULL AUTO_INCREMENT,
     CupType Varchar (20),
-    CupLocation varchar (40)
-)
+    CupLocation varchar (40),
+    PRIMARY KEY (CupID)
+);
 
 CREATE TABLE IF NONE EXISTS ProdInstance (
     CupID int FOREIGN KEY REFERENCES Cupboard(cupID),
@@ -26,4 +29,4 @@ CREATE TABLE IF NONE EXISTS ProdInstance (
     UserID int FOREIGN KEY REFERENCES [User](UserID),
     ExpiryDate date,
     Amount int
-)
+);
